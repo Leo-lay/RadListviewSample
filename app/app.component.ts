0ed6e8a4b2af0db090c9e81a76e2e6279f7c8685
Scleo <scleo@concordya.com>
2016-10-20 16:49:28 +0800

    sample init

---
+++ app/app.component.ts
@@ -0,0 +1,44 @@
+import {Component, OnInit} from "@angular/core";
+import { ObservableArray } from 'data/observable-array';
+import { ListViewEventData } from 'nativescript-telerik-ui/listview';
+@Component({
+    selector: "my-app",
+    templateUrl: "app.component.html",
+    styles: ['app.component.css']
+})
+export class AppComponent implements OnInit {
+    public counter: number = 16;
+    dataItems: ObservableArray<RadListviewSelectorModel>;
+    selectedItem: RadListviewSelectorModel;
+    ngOnInit() {
+        let tempList = [
+            { id: 0, name: '银行卡', isSelected: true },
+            { id: 1, name: '现金', isSelected: false },
+            { id: 2, name: '微信', isSelected: false },
+            { id: 3, name: '支付宝', isSelected: false },
+            { id: 4, name: '其他', isSelected: false }
+        ]
+        this.dataItems = new ObservableArray<RadListviewSelectorModel>(tempList);
+    }
+
+    public onItemTap(args: ListViewEventData) {
+        console.log(args.itemIndex);
+        this.selectedItem = this.dataItems.getItem(args.itemIndex);
+
+        for (let i = 0; i < this.dataItems.length; i++) {
+            let temp = this.dataItems.getItem(i);
+            if (this.selectedItem.id === temp.id) {
+                this.dataItems.getItem(i).isSelected = true;
+            } else {
+                this.dataItems.getItem(i).isSelected = false;
+            }
+        }
+    }
+}
+
+
+export class RadListviewSelectorModel {
+    id: number;
+    name: string;
+    isSelected: boolean;
+}
